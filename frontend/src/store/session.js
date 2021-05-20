5862308c9e6a77d1dd9830f9ff8a00e5725b0cee
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-05-20 13:59:03 -0700

    Finished setting up react actions and reducer for login and testing complete

---
+++ frontend/src/store/session.js
@@ -0,0 +1,55 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+// POJO action creator
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+// POJO action creator
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+
+  if (action.type === 'session/setUser') {
+    newState = Object.assign({}, state);
+    newState.user = action.payload;
+    return newState;
+  }
+
+  if (action.type === 'session/removeUser') {
+    newState = Object.assign({}, state);
+    newState.user = null;
+    return newState;
+  }
+
+  return state;
+};
+
+export default sessionReducer;
