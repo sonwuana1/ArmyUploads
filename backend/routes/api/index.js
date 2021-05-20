d19189ddecabd5d35f1f355fe5e73260d0f97649
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-05-20 09:24:07 -0700

    Login route finished and tested

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
 
 
 module.exports = router;
