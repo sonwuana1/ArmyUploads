08d4d9e3ea11642edc3e9fbae76a09cd80156d04
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-05-19 21:26:50 -0700

    Finished Phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+router.use('/api', apiRouter);
+
 
 module.exports = router;
