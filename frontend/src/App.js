78c060bc7ed2f96a359c80656e338ec0947534b3
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-05-20 15:05:07 -0700

    App now restores the user session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from "./store/session";
+
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+
+  return isLoaded && (
     <Switch>
       <Route path="/login">
         <LoginFormPage />
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,6 +32,13 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
