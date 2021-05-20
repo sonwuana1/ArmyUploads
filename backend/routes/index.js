e29494d4990ec377fb29178f95c9358e2165301e
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-05-20 10:07:54 -0700

    GET hello world test route was removed from backend/routes/index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+
 
 router.use('/api', apiRouter);
 
