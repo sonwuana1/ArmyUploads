b2c69ce63a8e2c54e62ee78e5a8a78831b543aec
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-05-20 09:38:38 -0700

    Get session user route finished and tested

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,5 +37,18 @@ router.delete('/', (_req, res) => {
 });
 
 
+// Restore session user
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+);
+
+
+
 
 module.exports = router;
